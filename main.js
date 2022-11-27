2732f51cbb544a527631e4e69a95d268352a8d25
axelriveroc <riverocoronelaxel@gmail.com>
2022-11-27 19:07:50 -0300

    Logré la lógica de armar la tarjeta con el input máscara en cada caso

--- main.js
+++ main.js
@@ -13,14 +13,50 @@ let arrClaveTarjeta = [];
 
 inputNumeroTarjeta.addEventListener('keydown', (e)=>{
     let number = e.key;
+    if (number === "Tab") {
+        return;
+    }
     e.preventDefault();
+    handleInput(arrNumerosTarjeta, number, templateNumeroTarjeta)
+    inputNumeroTarjeta.value = arrNumerosTarjeta.join('');
+})
 
-    arrNumerosTarjeta.push(number)
-    console.log(arrNumerosTarjeta)
-    handleInput(arrNumerosTarjeta)
+inputFechaVencimiento.addEventListener('keydown', (e)=>{
+    let number = e.key;
+    if (number === "Tab") {
+        return;
+    }
+    e.preventDefault();
+    handleInput(arrFechaVencimiento, number, templateFechaVencimiento)
+    inputFechaVencimiento.value = arrFechaVencimiento.join('');
+})
+
+inputClaveTarjeta.addEventListener('keydown', (e)=>{
+    let number = e.key;
+    if (number === "Tab") {
+        return;
+    }
+    e.preventDefault();
+    handleInput(arrClaveTarjeta, number, templateClaveTarjeta)
+    inputClaveTarjeta.value = arrClaveTarjeta.join('');
 })
 
 
-const handleInput = (arr)=>{
-    inputNumeroTarjeta.value = arr.join('');
+const handleInput = (arr, n, template)=>{
+
+    const valoresAceptados = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
+
+    if (n === 'Backspace') {
+        arr.pop();
+        return;
+    }
+    if (valoresAceptados.includes(n)) {
+        if (arr.length + 1 <= template.length) {
+            if (template[arr.length] === '-' || template[arr.length] === '/') {
+                arr.push( template[arr.length], n )
+            }else{
+                arr.push(n)
+            }
+        }
+    }
 }
